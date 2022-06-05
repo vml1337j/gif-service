1c4c7ea2c0211ac9f69d86f07913ee5bd3d7ebcd
vml1337j <vml133j@gmail.com>
2022-06-06 00:08:19 +0300

    add InvalidCurrencyCodeException

---
+++ src/main/java/vml1337j/gifservice/exception/InvalidCurrencyCodeException.java
@@ -0,0 +1,8 @@
+package vml1337j.gifservice.exception;
+
+public class InvalidCurrencyCodeException extends RuntimeException {
+
+    public InvalidCurrencyCodeException(String message) {
+        super(message);
+    }
+}

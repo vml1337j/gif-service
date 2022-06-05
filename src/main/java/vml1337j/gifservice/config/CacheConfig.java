050d3eed374a15eaad07ef3ede41e0165cfcf5c4
vml1337j <vml133j@gmail.com>
2022-06-06 00:06:09 +0300

    add cache config

---
+++ src/main/java/vml1337j/gifservice/config/CacheConfig.java
@@ -0,0 +1,29 @@
+package vml1337j.gifservice.config;
+
+import com.google.common.cache.CacheBuilder;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        return new ConcurrentMapCacheManager() {
+            @Override
+            protected Cache createConcurrentMapCache(String name) {
+                return new ConcurrentMapCache(
+                        name,
+                        CacheBuilder.newBuilder()
+                                .maximumSize(1)
+                                .build().asMap(),
+                        false
+                );
+            }
+        };
+    }
+}
